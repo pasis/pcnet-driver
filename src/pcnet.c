6818e1795bf46e2cef9492d6c54673eb7b62a77d
Dmitry Podgorny <pasis.ua@gmail.com>
2010-09-14 17:46:26 +0300

    Added simple linux module with descriptions

---
+++ src/pcnet.c
@@ -0,0 +1,28 @@
+/* pcnet.c: PCNet-PCI II/III Ethernet controller driver */
+
+#ifndef MODULE
+#define MODULE
+#endif
+
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Dmitry Podgorny <pasis.ua@gmail.com>");
+MODULE_DESCRIPTION("PCNet-PCI II/III Ethernet controller driver");
+MODULE_VERSION("dev");
+MODULE_LICENSE("GPL");
+
+static int __init pcnet_init(void)
+{
+	printk(KERN_INFO "pcnet: module has loaded");
+
+	return 1;
+}
+
+static void __exit pcnet_exit(void)
+{
+	printk(KERN_INFO "pcnet: module was unloaded");
+}
+
+module_init(pcnet_init);
+module_exit(pcnet_exit);
