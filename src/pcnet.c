b7b982a27bd45bd6d5496154e198795f5591bb39
Dmitry Podgorny <pasis.ua@gmail.com>
2012-10-03 17:10:23 +0300

    introduce functions for accessing registers

--- src/pcnet.c
+++ src/pcnet.c
@@ -90,6 +90,39 @@ struct pcnet_private {
 	void __iomem *base;
 };
 
+/* 16 most significant bits of all registers are undefined on reading and 
+ * must be set to 0 on writing (except of CSR88)
+ */
+
+#define read_csr(csr) pcnet_dummy_read_csr(ioaddr, csr)
+#define read_bcr(bcr) pcnet_dummy_read_bcr(ioaddr, bcr)
+#define write_csr(csr, val) pcnet_dummy_write_csr(ioaddr, csr, val)
+#define write_bcr(bcr, val) pcnet_dummy_write_bcr(ioaddr, bcr, val)
+
+static inline u32 pcnet_dummy_read_csr(void __iomem *ioaddr, u32 csr)
+{
+	iowrite32(csr, ioaddr + PCNET_RAP);
+	return ioread32(ioaddr + PCNET_RDP) & 0xffff;
+}
+
+static inline u32 pcnet_dummy_read_bcr(void __iomem *ioaddr, u32 bcr)
+{
+	iowrite32(bcr, ioaddr + PCNET_RAP);
+	return ioread32(ioaddr + PCNET_BDP) & 0xffff;
+}
+
+static inline void pcnet_dummy_write_csr(void __iomem *ioaddr, u32 csr, u32 val)
+{
+	iowrite32(csr, ioaddr + PCNET_RAP);
+	iowrite32(val & 0xffff, ioaddr + PCNET_RDP);
+}
+
+static inline void pcnet_dummy_write_bcr(void __iomem *ioaddr, u32 bcr, u32 val)
+{
+	iowrite32(bcr, ioaddr + PCNET_RAP);
+	iowrite32(val & 0xffff, ioaddr + PCNET_BDP);
+}
+
 static int pcnet_dummy_open(struct net_device *ndev)
 {
 	return 0;
--- src/pcnet.h
+++ src/pcnet.h
@@ -8,3 +8,11 @@
 enum {
 	PCNET_IOSIZE_LEN = 0x20,
 };
+
+enum {
+	PCNET_APROM = 0x00,
+	PCNET_RDP = 0x10,
+	PCNET_RAP = 0x14,
+	PCNET_RESET = 0x18,
+	PCNET_BDP = 0x1C,
+};
